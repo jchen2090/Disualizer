94bfaff49609e85212ccd9ef6a27df482e599d55
Jason <jasonchen5044@gmail.com>
2024-01-16 01:49:52 -0500

    Loading page

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,16 +1,18 @@
 import Die from "../components/Die";
 import { useDashboardContext } from "../hooks/useDashboardContext";
+import Loading from "./Loading";
 
 export default function Dashboard() {
   const { isLoading, data, setIsLoading } = useDashboardContext();
 
   console.log(data);
   setTimeout(() => {
+    //TODO: Unsure when to make it load
     setIsLoading(false);
   }, 2000);
 
   if (isLoading) {
-    return <div>Loading</div>;
+    return <Loading />;
   }
 
   return (
